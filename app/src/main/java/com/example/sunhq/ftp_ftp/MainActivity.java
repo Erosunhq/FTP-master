53f819a2885be712f02bd429f1fed06d80f5cf00
Erosunhq <604352908@qq.com>
2018-05-03 15:24:59 +0800

    单个文件夹下批量下载所有图片

--- app/src/main/java/com/example/sunhq/ftp_ftp/MainActivity.java
+++ app/src/main/java/com/example/sunhq/ftp_ftp/MainActivity.java
@@ -29,8 +29,13 @@ public class MainActivity extends AppCompatActivity {
     // 定义文件的存取路径(分类存放在不同的文件夹下面)
     public static final String HOME_SHOW = "/FTP/images/";  //家装展示图片路径
     public static final String CORPORATE_HONOR = "/FTP/image/";  //企业荣誉
-    public static final String ENGINEERING_CASE = "/FTP/images/";  //工程案列
+    public static final String ENGINEERING_CASE = "/FTP/project/";  //工程案列
 
+    String[] serverFolder = new String[]{HOME_SHOW,CORPORATE_HONOR,ENGINEERING_CASE+"parameter1/"
+            ,ENGINEERING_CASE+"parameter2/",ENGINEERING_CASE+"parameter3/",ENGINEERING_CASE+"parameter4/"};
+
+    //本地存放的文件夹
+    String localFolder = "/mnt/sdcard/Picture/";
     // **********************************************
 
     // 创建线程池对内存进行优化处理
@@ -82,29 +87,53 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 }).start();*/
-
                 executorService.execute(new Runnable() {
                     @Override
                     public void run() {
                         // 下载
                         try {
-                            String[] serverPath =  new FTP().JudgeFile(HOME_SHOW);  // 服务端的文件路径
-                            for (int i = 0; i<serverPath.length;i++){
-                                //单文件下载 (服务器上文件的路径, 存放的本地路径, 下载到本地后要保存的文件名称(可以跟源文件名不一样), 下载监听器)
-                                new FTP().downloadSingleFile(HOME_SHOW+serverPath[i],"/mnt/sdcard/sunhq/",serverPath[i],new FTP.DownLoadProgressListener(){
-                                    @Override
-                                    public void onDownLoadProgress(String currentStep, long downProcess, File file) {
-                                        Log.d(TAG, currentStep);
-                                        if(currentStep.equals(MainActivity.FTP_DOWN_SUCCESS)){
-                                            Log.d(TAG, "-----下载--成功");
-                                        } else if(currentStep.equals(MainActivity.FTP_DOWN_LOADING)){
-                                            Log.d(TAG, "-----下载---"+downProcess + "%");
+                            for (int j = 0; j < serverFolder.length; j++) {
+                                switch (j){
+                                    case 0:
+                                        localFolder = "/mnt/sdcard/Picture/"+"images/";
+                                        break;
+                                    case 1:
+                                        localFolder = "/mnt/sdcard/Picture/"+"image/";
+                                        break;
+                                    case 2:
+                                        localFolder = "/mnt/sdcard/Picture/"+"project/parameter1/";
+                                        break;
+                                    case 3:
+                                        localFolder = "/mnt/sdcard/Picture/"+"project/parameter2/";
+                                        break;
+                                    case 4:
+                                        localFolder = "/mnt/sdcard/Picture/"+"project/parameter3/";
+                                        break;
+                                    case 5:
+                                        localFolder = "/mnt/sdcard/Picture/"+"project/parameter4/";
+                                        break;
+                                }
+                                String[] serverPath = new FTP().JudgeFile(serverFolder[j]);  // 服务端的文件路径
+                                for (int i = 0; i < serverPath.length; i++) {
+                                    //单文件下载 (服务器上文件的路径, 存放的本地路径, 下载到本地后要保存的文件名称(可以跟源文件名不一样), 下载监听器)
+                                    new FTP().downloadSingleFile(serverFolder[j] + serverPath[i], localFolder, serverPath[i], new FTP.DownLoadProgressListener() {
+                                        @Override
+                                        public void onDownLoadProgress(String currentStep, long downProcess, File file) {
+                                            Log.d(TAG, currentStep);
+                                            if (currentStep.equals(MainActivity.FTP_DOWN_SUCCESS)) {
+                                                Log.d(TAG, "-----下载--成功");
+                                            } else if (currentStep.equals(MainActivity.FTP_DOWN_LOADING)) {
+                                                Log.d(TAG, "-----下载---" + downProcess + "%");
 
+                                            }
+                                            downProcess = 0;   //对不同的文件,进度重新置0
                                         }
-                                        downProcess = 0;   //对不同的文件,进度重新置0
+                                    });
+                                    if (i == serverPath.length) {
+                                        Log.d(TAG, "该文件夹下图片全部下载完成!!!");
                                     }
-                                });
-                            }
+                                }
+                        }
                         } catch (Exception e) {
                             e.printStackTrace();
                     }
